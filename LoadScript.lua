90ade704ddecdf0ca8da5c18d21005f4bc47619c
Mtrx77 <104329629+Mtrx77@users.noreply.github.com>
2023-08-09 13:30:35 -0600

    Update LoadScript.lua

--- LoadScript.lua
+++ LoadScript.lua
@@ -2,7 +2,7 @@ return{
     ["ScriptedGames"] = {
         [1348402608] = "https://raw.githubusercontent.com/Mtrx77/rzt/main/a.lua",
         [1119466531] = "https://raw.githubusercontent.com/Mtrx77/scriptsslhub/main/legendsspeed.lua",
-        [2331157949] = "https://raw.githubusercontent.com/Mtrx77/scriptsslhub/main/SuperEvolution.lua"
+        [2331157949] = "https://raw.githubusercontent.com/Mtrx77/scriptsslhub/main/SuperEvolution.lua",
         [4099570905] = "https://raw.githubusercontent.com/Mtrx77/scriptsslhub/main/afsx.lua"
     },
    LoadScript = function (ScriptToload)
